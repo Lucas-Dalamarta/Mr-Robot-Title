ff241142e33c94c7ea6102d6732f4d936143f4a0
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-11-28 01:27:37 -0300

    Added glitch animation

--- src/Components/Title/styles.ts
+++ src/Components/Title/styles.ts
@@ -1,14 +1,43 @@
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
+
 
 export const Container = styled.div`
   height: 100vh;
-
+  
   display: flex;
   justify-content: center;
   align-items: center;
 `
 
+const glitch = keyframes`
+  0% {
+    transform: translateX(-2px);
+  }
+
+  10% {
+    transform: translateY(1px);
+  }
+
+  50% {
+    transform: translateY(-1px);
+  }
+
+  90% {
+    transform: translateX(1px);
+  }
+
+  100% {
+    transform: translateY(3px);
+  }
+`
+
 export const Text = styled.img`
-  width: 25rem;
+  width: 70rem;
   margin-bottom: 15rem;
+
+  @media (max-width: 800px) {
+    width: 25rem;
+  }
+
+  animation: ${glitch} 5s ease-in infinite;
 `
